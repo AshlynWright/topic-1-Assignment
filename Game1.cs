063069152cfc28912d48959876fe24ed4acd46a9
Ashlyn Wright <362581399@GoTVDSB.ca>
2025-11-26 09:09:08 -0500

    finish at home

--- Game1.cs
+++ Game1.cs
@@ -9,6 +9,13 @@ namespace topic_1_Assignment
         private GraphicsDeviceManager _graphics;
         private SpriteBatch _spriteBatch;
 
+        Rectangle window;
+
+        Texture2D dinoTexture;
+        Texture2D sonictexture;
+        //add more textures here
+        //add more textures here
+
         public Game1()
         {
             _graphics = new GraphicsDeviceManager(this);
@@ -19,6 +26,14 @@ namespace topic_1_Assignment
         protected override void Initialize()
         {
             // TODO: Add your initialization logic here
+            window = new Rectangle(0, 0, 800, 600);
+
+            _graphics.PreferredBackBufferWidth = window.Width;
+            _graphics.PreferredBackBufferHeight = window.Height;
+            _graphics.ApplyChanges();
+
+            this.Window.Title = "My first mongame project";
+
 
             base.Initialize();
         }
@@ -28,6 +43,11 @@ namespace topic_1_Assignment
             _spriteBatch = new SpriteBatch(GraphicsDevice);
 
             // TODO: use this.Content to load your game content here
+            dinoTexture = Content.Load<Texture2D>("dino");
+            sonictexture = Content.Load<Texture2D>("sonic");
+            //load more textures here
+            //load more textures here
+
         }
 
         protected override void Update(GameTime gameTime)
@@ -42,9 +62,28 @@ namespace topic_1_Assignment
 
         protected override void Draw(GameTime gameTime)
         {
-            GraphicsDevice.Clear(Color.CornflowerBlue);
+            GraphicsDevice.Clear(Color.Lavender);
 
             // TODO: Add your drawing code here
+            _spriteBatch.Begin();
+
+            _spriteBatch.Draw(dinoTexture, new Vector2(10, 10), Color.White);
+            _spriteBatch.Draw(dinoTexture, new Vector2(100, 200), Color.White);
+            _spriteBatch.Draw(sonictexture, new Vector2(500, 400), Color.White);
+            _spriteBatch.Draw(sonictexture, new Vector2(300, 100), Color.White);
+            _spriteBatch.Draw(sonictexture, new Vector2(200, 300), Color.White);
+            _spriteBatch.Draw(dinoTexture, new Vector2(600, 150), Color.White);
+            _spriteBatch.Draw(dinoTexture, new Vector2(400, 350), Color.White);
+            //draw more textures here
+            //draw more textures here
+
+
+
+
+
+
+            _spriteBatch.End();
+
 
             base.Draw(gameTime);
         }
